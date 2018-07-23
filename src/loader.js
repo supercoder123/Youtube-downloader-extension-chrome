8795728adf423ea69777a3b1ddf4a2c7ad916df0
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-24 01:03:31 +0530

    added background script for download

--- src/loader.js
+++ src/loader.js
@@ -12,5 +12,14 @@ window.addEventListener("message", function(e) {
   console.log(e.data);
   const fileExtension = e.data.type.split("/")[1].split(";")[0];
   const downloadFileName = e.data.name + "." + fileExtension;
-  chrome.downloads.download({ url: e.data.url, filename: downloadFileName });
+  console.log(downloadFileName, chrome);
+
+  chrome.runtime.sendMessage(
+    { name: downloadFileName, url: e.data.url },
+    function(res) {
+      console.log("res", res);
+    }
+  );
+
+  //chrome.downloads.download({ url: e.data.url, filename: downloadFileName });
 });
