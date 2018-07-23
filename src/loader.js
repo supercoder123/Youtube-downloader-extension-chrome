ac6bf2e2ed0b64c1940f4314fca73da23e56b987
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-23 19:41:06 +0530

    injected button to html

---
+++ src/loader.js
@@ -0,0 +1,9 @@
+let script = document.createElement("script");
+
+script.src = chrome.extension.getURL("src/youtubeDownloader.js");
+
+script.onload = function() {
+  this.remove();
+};
+
+document.head.appendChild(script);
--- src/youtubeDownloader.js
+++ src/youtubeDownloader.js
@@ -1,6 +1,31 @@
-console.log("Our extension has loaded");
 // ytplayer.config.args.url_encoded_fmt_stream_map
 
-let videoUrl = window.ytplayer.config.args.url_encoded_fmt_stream_map.split(
-  ","
-);
+window.onload = function() {
+  //   let videoUrl = window.ytplayer.config.args.url_encoded_fmt_stream_map
+  //     .split(",")
+  //     .map(item => {
+  //       return item.split("&").reduce((pre, cur) => {
+  //         cur  = cur.split("=");
+  //         return Object.assign(pre, {[cur[0]]:decodeURIComponent(cur[1])});
+  //       },{});
+  //     });
+
+  let videoUrl = window.ytplayer.config.args.url_encoded_fmt_stream_map
+    .split(",")
+    .map(item => {
+      return item.split("&").reduce((pre, cur) => {
+        cur = cur.split("=");
+        return Object.assign(pre, { [cur[0]]: decodeURIComponent(cur[1]) });
+      }, {});
+    });
+
+  console.log("Our extension has loaded", videoUrl);
+
+  const container = document.getElementById("menu-container");
+  const btn = document.createElement("button");
+  btn.className = "style-scope ytd-menu-renderer force-icon-button style-text";
+  btn.setAttribute("role", "button");
+  btn.innerText = "Download";
+
+  container.prepend(btn);
+};
